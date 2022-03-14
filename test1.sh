07f8cd983419c831b097ca4a6959745ee7d7d6eb
Lawry Sorenson <lawrysorenson@gmail.com>
2022-03-14 16:08:25 -0600

    Initial code commit

---
+++ test1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --time=05:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=60192M   # memory per CPU core
+#SBATCH -J "hwr 1"   # job name
+#SBATCH --gpus=1
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+source /fslhome/pipoika3/anaconda3/etc/profile.d/conda.sh
+conda activate unsup
+
+python3 unsup.py
+
